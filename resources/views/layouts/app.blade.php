a1b48b1669c3d8086734d9152ff95e6133bebe57
todorf <todor.filipovic18@gmail.com>
2026-03-04 20:04:52 +0100

    task-3 - add bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <main>
@@ -18,7 +19,11 @@
                 {{ session('error') }}
             </div>
         @endif
-        
+
+        @if (auth()->user())
+            @include('layouts.components.sidebar')
+        @endif
+
         @yield('content')
     </main>
     @livewireScripts
