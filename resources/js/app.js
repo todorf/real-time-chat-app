4cce91bbd96c1e0dee583244c0429d77d178ed4c
todorf <todor.filipovic18@gmail.com>
2026-03-09 20:08:27 +0100

    main - import bootstrap icons min

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import '../css/app.css';
